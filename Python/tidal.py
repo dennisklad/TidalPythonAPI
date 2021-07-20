84f8726003505ffae03a50d49645ca801a1f5d3d
dennisklad <kladis@campus.tu-berlin.de>
2021-07-20 14:26:24 +0200

    final version to show new albums

--- Python/tidal.py
+++ Python/tidal.py
@@ -1,28 +1,46 @@
 import tidalapi
 import xlsxwriter
 import datetime
-import os
 import glob
+import os
+import pandas as pd
 
-x = str(datetime.datetime.now())
-outWorkbook = xlsxwriter.Workbook('albums/albums'+x[:10]+'.xlsx')
-outSheet = outWorkbook.add_worksheet('albums')
-outSheet.write('A1', "Artist")
-outSheet.write('B1', "Title")
-outSheet.write('C1', "Release")
-outSheet.write('D1', "Tracks")
-outSheet.write('E1', "Duration")
-session = tidalapi.Session()
+def getTidalAlbums():
+    #Log in Tidal with Browser and get albums
+    session = tidalapi.Session()
+    session.login_oauth_simple()
+    return tidalapi.Favorites(session, session.user.id).albums()
 
-session.login_oauth_simple()
-favorites = tidalapi.Favorites(session, session.user.id)
-albums = favorites.albums()
-for item in range(len(albums)):
-    partist =  ' & '.join([str(art.name) for art in albums[item].artists])
-    outSheet.write(item+1, 0, partist)
-    outSheet.write(item+1, 1, albums[item].name)
-    outSheet.write(item+1, 2, albums[item].release_date.year)
-    outSheet.write(item+1, 3, albums[item].num_tracks)
-    outSheet.write(item+1, 4, int(albums[item].duration/60))
+def writeTidalAlbums(albums):
+    #Open File to write
+    outWorkbook = xlsxwriter.Workbook('albums/albums'+str(datetime.datetime.now())[:10]+'.xlsx')
+    outSheet = outWorkbook.add_worksheet('albums')
+    #Create headers
+    [outSheet.write(0, idx, data) for idx, data in enumerate(["Artist","Title","Release","Tracks","Duration"])]
+    #Write data to file
+    for item in range(len(albums)):
+        partist =  ' & '.join([str(art.name) for art in albums[item].artists])
+        outSheet.write(item+1, 0, partist)
+        outSheet.write(item+1, 1, albums[item].name)
+        outSheet.write(item+1, 2, albums[item].release_date.year)
+        outSheet.write(item+1, 3, albums[item].num_tracks)
+        outSheet.write(item+1, 4, int(albums[item].duration/60))
+    outWorkbook.close()
 
-outWorkbook.close()
+def compareNewAlbums():
+    #Compare new rows that were added in the latest iteration
+    files = os.listdir('albums/')
+    fnew = files[len(files)-1]
+    fold = files[len(files)-2]
+    print ("fnew: " + fnew)
+    print ("fold: " + fold)
+    dfnew=pd.read_excel('albums/' + fnew)
+    dfold=pd.read_excel('albums/' + fold)
+    print(f"New file has {len(dfnew)} rows, old file has {len(dfold)} rows. You added {len(dfnew)-len(dfold)} albums since the last update.\n")
+    merged = dfnew.append(dfold)
+    merged = merged.drop_duplicates(keep=False).sort_index()
+    print (merged)
+    merged.to_excel("unique.xlsx")
+
+writeTidalAlbums(getTidalAlbums())
+compareNewAlbums()
